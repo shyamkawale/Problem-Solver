0bc0976eff67319469694e0398c5d61e36dba405
Shyam Kawale <shyamvilaskawale@gmail.com>
2024-09-03 09:53:21 +0530

    added few dataconvertors

--- src/DemoProblem.java
+++ src/DemoProblem.java
@@ -9,23 +9,27 @@ public class DemoProblem extends ProblemSolver {
     @Override
     public void processParameters(String[] args) {
         int[] intArray = DataConvertor.toIntArray(args[0]);
+        List<Integer> list = DataConvertor.toList(args[0], Integer.class);
         String string = args[1];
         int intNum = DataConvertor.toInt(args[2]);
         ListNode linkedList = ListWrapper.stringToListNode(args[3]);
         TreeNode tree = TreeWrapper.stringToTreeNode(args[4]);
+        List<List<String>> listOfList = DataConvertor.toListOfList(args[5], String.class);
 
-        functionName(intArray, string, intNum, linkedList, tree);
+        functionName(intArray, string, intNum, linkedList, tree, list, listOfList);
     }
 
     public static void main(String[] args) {
         new DemoProblem().readInput();
     }
 
-    public static void functionName(int[] arr, String string, int num, ListNode linkedList, TreeNode tree) {
+    public static void functionName(int[] arr, String string, int num, ListNode linkedList, TreeNode tree, List<Integer> list, List<List<String>> listOfList) {
         System.out.println(Arrays.toString(arr));
         System.out.println(string);
         System.out.println(num);
         ListWrapper.prettyPrintLinkedList(linkedList);
         TreeWrapper.prettyPrintTree(tree);
+        System.out.println(list);
+        System.out.println(listOfList);
     }
 }
--- src/Helpers/DataConvertor.java
+++ src/Helpers/DataConvertor.java
@@ -4,30 +4,70 @@ import java.lang.reflect.Type;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
 public class DataConvertor {
     private static final Gson gson = new Gson();
 
     public static <T> T fromJson(String json, Class<T> clazz) {
-        return gson.fromJson(json, clazz);
+        try {
+			return gson.fromJson(json, clazz);
+		} catch (JsonSyntaxException e) {
+			System.out.println("Error while converting this json: " + json + "error message: " + e.getMessage());
+            return null;
+		}
     }
 
-    public static <T> List<T> fromJsonArray(String json, Class<T[]> clazz) {
-        Type type = TypeToken.getParameterized(List.class, clazz).getType();
-        return gson.fromJson(json, type);
+    public static <T> List<T> fromJson(String json, Type type) {
+        try {
+			return gson.fromJson(json, type);
+		} catch (JsonSyntaxException e) {
+			System.out.println("Error while converting this json: " + json + "error message: " + e.getMessage());
+            return null;
+		}
+    }
+
+    public static <T> List<List<T>> toListOfList(String json, Class<T> clazz) {
+        try {
+			Type type = TypeToken.getParameterized(List.class, TypeToken.getParameterized(List.class, clazz).getType()).getType();
+			return gson.fromJson(json, type);
+		} catch (JsonSyntaxException e) {
+			System.out.println(e.getMessage());
+            return null;
+		}
+    }
+
+    public static <T> List<T> toList(String json, Class<T> clazz) {
+        try {
+			Type type = TypeToken.getParameterized(List.class, clazz).getType();
+			return gson.fromJson(json, type);
+		} catch (JsonSyntaxException e) {
+			System.out.println(e.getMessage());
+            return null;
+		}
+    }
+
+    public static <T> T[] toTypeArray(String json, Class<T> clazz){
+        try {
+			Type type = TypeToken.getArray(clazz).getType();
+			return gson.fromJson(json, type);
+		} catch (JsonSyntaxException e) {
+			System.out.println(e.getMessage());
+            return null;
+		}
     }
 
     public static int[] toIntArray(String json) {
         return fromJson(json, int[].class);
     }
 
-    public static String[] toStringArray(String json) {
-        return fromJson(json, String[].class);
-    }
-
     public static char[] toCharArray(String json){
         return fromJson(json, char[].class);
+    }
+
+    public static double[] toDoubleArray(String json){
+        return fromJson(json, double[].class);
     }
 
     public static int[][] to2DIntArray(String json) {
@@ -48,5 +88,9 @@ public class DataConvertor {
 
     public static float toFloat(String json){
         return fromJson(json, float.class);
+    }
+
+    public static double toDouble(String json){
+        return fromJson(json, double.class);
     }
 }
