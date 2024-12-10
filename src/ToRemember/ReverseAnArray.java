c0fe899486dff2a3e2ea164482f7d97a484ccf1c
Shyam Kawale <shyamvilaskawale@gmail.com>
2024-12-10 22:33:48 +0530

    reverse and array fixed

--- src/ToRemember/ReverseAnArray.java
+++ src/ToRemember/ReverseAnArray.java
@@ -6,12 +6,12 @@ public class ReverseAnArray {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5 };
 
-        reverseArray(0, arr.length - 1, arr);
+        reverseArray(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
 
     }
     
-    private static void reverseArray(int start, int end, int[] arr) {
+    private static void reverseArray(int[] arr, int start, int end) {
         while (start < end) {
             int temp = arr[start];
             arr[start] = arr[end];
